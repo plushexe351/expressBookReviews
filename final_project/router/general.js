5f9c0f0a6d4f656460a07c921b2840d17b5b9a3d
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2024-04-18 02:31:41 +0530

    brought back accidentally deleted code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,6 +1,24 @@
 const express = require('express');
 let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
 const public_users = express.Router();
+
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (users.some(user => user.username === username)) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+  users.push({ username, password });
+
+  return res.status(200).json({ message: "User successfully registered" });
+});
 
 // Promise callback to get book list
 const getBookList = () => {
